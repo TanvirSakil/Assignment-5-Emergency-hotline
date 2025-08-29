e6012d9d9929f28c8e551203b8db264c6ee4522b
TanvirSakil <tanvirbt14@gmail.com>
2025-08-29 17:03:57 +0600

    Adding call button eventListener and all functions

--- script.js
+++ script.js
@@ -14,3 +14,68 @@ for (let i = 0; i < len; i++) {
 }
 
 
+
+// call button function-----
+
+let coins = 100;
+const coinTotal = document.getElementById('coinCount');
+
+
+const buttons = document.getElementsByClassName('callBtn');
+
+
+for (const button of buttons) {
+  button.addEventListener('click', function () {
+
+    const card = this.closest('.card');
+    const serviceName = card.getElementsByClassName('serviceTitle')[0].innerText;
+    const serviceNum = card.getElementsByClassName('serviceNumber')[0].innerText;
+
+
+    if (coins < 20) {
+      alert("Not enough coins to make a call!");
+      return;
+    }
+
+
+    coins -= 20;
+    coinTotal.innerText = coins;
+
+
+    alert(' 📞Calling ' + serviceName + " at " + serviceNum);
+
+    const time = new Date().toLocaleTimeString();
+
+    const history = document.getElementById('callHistory');
+    const newAdd = document.createElement('div');
+    const newAdd2 = document.createElement('div');
+    const newAdd3 = document.createElement('div');
+
+    newAdd.className = 'flex justify-between items-center  p-3'
+    newAdd2.className = 'mx-3 mt-4 '
+
+
+
+    newAdd2.innerHTML = ` <strong>${serviceName}</strong><br>
+          Number: ${serviceNum}`;
+
+    newAdd3.innerHTML = ` ${time}`;
+
+    callHistory.appendChild(newAdd);
+    newAdd.appendChild(newAdd2);
+    newAdd.appendChild(newAdd3);
+
+
+
+
+  });
+}
+clearHistoryBtn.addEventListener("click", function () {
+  callHistory.innerHTML = ""; // remove all history
+});
+
+
+
+
+
+
